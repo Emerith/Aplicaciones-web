f4881cfa73fd3dab61c015be3fd5c000c9749f61
Emerith <isc_ramirez.j@tesvb.edu.mx>
2018-09-18 07:36:32 -0500

    template

---
+++ Views/Template.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace Views;
+
+
+use http\Url;
+
+class Template
+{
+    public static function header()
+    {
+       ?>
+        <!DOCTYPE html>
+        <html>
+        <head>
+
+        </head>
+
+            <body>
+                    <nav>
+
+                    </nav>
+
+            </body>
+        </html>
+        <?php
+    }
+}
